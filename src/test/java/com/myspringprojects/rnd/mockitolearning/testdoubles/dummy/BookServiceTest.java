e17f2566eb4e5b2b7be530335c5a4d7e0dbe72cb
P2000948290 <Hareesh.Ravindran@in.ey.com>
2021-06-30 15:02:29 +0530

    Introduced the Mockito framework and mocked the Fake and Dummy objects.

--- src/test/java/com/myspringprojects/rnd/mockitolearning/testdoubles/dummy/BookServiceTest.java
+++ src/test/java/com/myspringprojects/rnd/mockitolearning/testdoubles/dummy/BookServiceTest.java
@@ -1,10 +1,12 @@
 package com.myspringprojects.rnd.mockitolearning.testdoubles.dummy;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +26,32 @@ class BookServiceTest {
         bookService.addBook(new Book("1235", "JUnit5 in Action", 200, LocalDate.now()));
 
         // Verify if the findAll method on the service returns the number of books added.
+        assertEquals(2, bookService.findNoOfBooks());
+    }
+
+    @Test
+    @DisplayName("Dummy Test Double with Mockito")
+    void testDummyTestDoubleWithMockito() {
+
+        // First create a mock object for the BookRepository and the EmailService.
+        // Note that, we do not have to implement these classes manually.
+        // Mockito wil dynamically provide a mock implementation.
+        BookRepository bookRepository = Mockito.mock(BookRepository.class);
+        EmailService emailService = Mockito.mock(EmailService.class);
+
+        BookService bookService = new BookService(bookRepository, emailService);
+
+        Book book1 = new Book("1234", "Mockito in Action", 250, LocalDate.now());
+        Book book2 = new Book("1235", "JUnit5 in Action", 200, LocalDate.now());
+
+        Collection<Book> books = List.of(book1, book2);
+
+        // Stub the findAll() method of the bookRepository.
+        Mockito.when(bookRepository.findAll()).thenReturn(books);
+
+        // Verify that when the BookService.findNoOfBooks() is called, it internally uses the
+        // value returned by the BookRepository. Since, we stubbed the findAll() method of the repository
+        // to return 2 hard coded books, the same books will be used here in this execution.
         assertEquals(2, bookService.findNoOfBooks());
     }
 }
--- src/test/java/com/myspringprojects/rnd/mockitolearning/testdoubles/fake/BookServiceTest.java
+++ src/test/java/com/myspringprojects/rnd/mockitolearning/testdoubles/fake/BookServiceTest.java
@@ -2,8 +2,11 @@ package com.myspringprojects.rnd.mockitolearning.testdoubles.fake;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,4 +26,26 @@ class BookServiceTest {
         assertEquals(2, bookService.findNoOfBooks());
     }
 
+    @Test
+    @DisplayName("Fake Test Double with Mockito")
+    void testFakeWithMockito() {
+
+        // First create a mock object for the BookRepository. Note that, we do not have to implement
+        // this class manually. Mockito wil dynamically provide a mock implementation.
+        BookRepository bookRepository = Mockito.mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        Book book1 = new Book("1234", "Mockito in Action", 250, LocalDate.now());
+        Book book2 = new Book("1235", "JUnit5 in Action", 200, LocalDate.now());
+
+        Collection<Book> books = List.of(book1, book2);
+
+        // Stub the findAll() method of the bookRepository.
+        Mockito.when(bookRepository.findAll()).thenReturn(books);
+
+        // Verify that when the BookService.findNoOfBooks() is called, it internally uses the
+        // value returned by the BookRepository. Since, we stubbed the findAll() method of the repository
+        // to return 2 hard coded books, the same books will be used here in this execution.
+        assertEquals(2, bookService.findNoOfBooks());
+    }
 }
